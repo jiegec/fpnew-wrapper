85d4f5c09162bd5877a8bac2777c5345c7448195
Jiajie Chen <c@jia.je>
2020-04-06 19:38:43 +0800

    Add vectorial test

--- src/test/scala/fpnew/FPNewTest.scala
+++ src/test/scala/fpnew/FPNewTest.scala
@@ -4,6 +4,21 @@ import chisel3._
 import chisel3.iotesters._
 
 class FPNewTester(c: FPNew) extends PeekPokeTester(c) {
+  def waitAndExpect(num: String) {
+    poke(c.in.valid, true)
+    poke(c.out.ready, true)
+    step(1)
+    while (peek(c.in.ready) == 0) {
+      step(1)
+    }
+    while (peek(c.out.valid) == 0) {
+      step(1)
+    }
+    expect(c.out.bits.out, num.U)
+    poke(c.out.ready, false)
+    poke(c.in.valid, false)
+  }
+
   // 2 * 3 + 4 = 10
   // NaN-boxed
   // 2.0
@@ -16,11 +31,22 @@ class FPNewTester(c: FPNew) extends PeekPokeTester(c) {
   poke(c.in.bits.srcFormat, FPFloatFormat.Fp32)
   poke(c.in.bits.dstFormat, FPFloatFormat.Fp32)
   poke(c.in.bits.tag, 1)
-  poke(c.in.valid, true)
-  poke(c.out.ready, true)
-  step(20)
   // 10.0
-  expect(c.out.bits.out, "hFFFFFFFF41200000".U)
+  waitAndExpect("hFFFFFFFF41200000")
+
+  // packed 2 fp32
+  // 2.0, 3.0
+  poke(c.in.bits.in1, "h4000000040400000".U)
+  // 4.0, 5.0
+  poke(c.in.bits.in2, "h4080000040A00000".U)
+  // 6.0, 7.0
+  poke(c.in.bits.in3, "h40C0000040E00000".U)
+  poke(c.in.bits.op, FPOp.FMADD)
+  poke(c.in.bits.vectorial, true)
+  poke(c.in.bits.srcFormat, FPFloatFormat.Fp32)
+  poke(c.in.bits.dstFormat, FPFloatFormat.Fp32)
+  // 14.0, 22.0
+  waitAndExpect("h4160000041B00000")
 }
 
 class FPNewUnitTest extends ChiselFlatSpec {
