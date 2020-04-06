6938b86d216e85417f9336dd66de4bbe0da1adb7
Jiajie Chen <c@jia.je>
2020-04-06 18:18:10 +0800

    Connect in/out channels

--- src/main/scala/fpnew/FPNew.scala
+++ src/main/scala/fpnew/FPNew.scala
@@ -2,16 +2,31 @@ package fpnew
 
 import chisel3._
 import chisel3.util.Cat
+import chisel3.util.Decoupled
+
+class FPInput(val flen: Int) extends Bundle {
+  val in1 = UInt(flen.W)
+  val in2 = UInt(flen.W)
+  val in3 = UInt(flen.W)
+}
+
+class FPOutput(val flen: Int) extends Bundle {
+  val out = UInt(flen.W)
+}
 
 class FPNew(flen: Int) extends MultiIOModule {
-  val in1 = IO(Input(UInt(flen.W)))
-  val in2 = IO(Input(UInt(flen.W)))
-  val in3 = IO(Input(UInt(flen.W)))
+  val in = IO(Flipped(Decoupled(new FPInput(flen))))
+  val out = IO(Decoupled(new FPOutput(flen)))
 
   val blackbox = Module(new FPNewBlackbox(flen = flen))
   blackbox.io.clk_i := clock
   blackbox.io.rst_ni := ~reset.asBool()
-  blackbox.io.operands_i := Cat(in1, in2, in3)
+  blackbox.io.operands_i := Cat(in.bits.in1, in.bits.in2, in.bits.in3)
+  blackbox.io.in_valid_i := in.valid
+  in.ready := blackbox.io.in_ready_o
+  out.bits.out := blackbox.io.result_o
+  out.valid := blackbox.io.out_valid_o
+  blackbox.io.out_ready_i := out.ready
 }
 
 object FPNewMain extends App {
